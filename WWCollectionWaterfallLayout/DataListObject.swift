03caf54fef16294ba51c8b00fc3595f122ca1ef9
fairythm <32063429+fairythm@users.noreply.github.com>
2019-06-17 15:01:06 +0800

    establish

---
+++ WWCollectionWaterfallLayout/DataListObject.swift
@@ -0,0 +1,13 @@
+//
+//  DataListObject.swift
+//  WWCollectionWaterfallLayout
+//
+//  Created by YU on 11/6/2019.
+//  Copyright © 2019 Tidus. All rights reserved.
+//
+
+import UIKit
+
+class DataListObject: NSObject {
+
+}
